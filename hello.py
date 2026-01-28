6a07dcc614065f2531194dac384cc8fb265deb37
Ranganath Gowda <hemanthranganathgowda@gmail.com>
2026-01-28 21:57:01 +0530

    if else: even odd program ,age

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hello GitHub")
+x= 12
+y=33
+sum=x+y
+print(sum)
+print(int(input("enter  an age of a person")))
+if (x+y)>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")
