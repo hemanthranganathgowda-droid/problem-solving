af2c01bdfd69bdeafc7985c04c904680e7fb0445
hemanthranganathgowda-droid <hemanthranganathgowda@gmail.com>
2026-02-20 13:26:54 +0530

    arithmatic and logical operators

---
+++ 4th day.py
@@ -0,0 +1,46 @@
+"""x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+if(x>10 and y>10):
+    print("both are greater than 10")
+else:
+    print("not greater than 10")"""
+
+
+"""x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+if(x>10 or y>10):
+    print("both are greater than 10")
+else:
+    print("not greater than 10")"""
+
+"""x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+if(not (y>10)):
+    print("both are greater than 10")
+else:
+    print("not greater than 10")"""
+
+"""age=int(input("enter your age"))
+if(age>=18):
+    print("you are an adult")
+else:
+    print("you are minor ")"""
+
+"""str1=input("enter the string")
+print('a' in str1)
+print('a' not in str1)"""
+
+
+
+"""my_list=[1,2,3,4,5]
+print( 8 in my_list)"""
+
+
+"""x=10
+y=20
+print(x!=y)
+print(x**y)
+print(x/y)"""
+
+
+
